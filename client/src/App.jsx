3cd5a7c524f2070622d02b3090df404d79dc44f2
Ammar <ammar.alnatsheh@gmail.com>
2019-05-13 12:19:58 -0700

    start new react project

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount () {
+    }
+
+    render() {
+        return (
+        <div className='App'>
+        Hello World
+        </div>);
+    }
+}
+
+export default App;
