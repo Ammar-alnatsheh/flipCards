3bdc4ebb37a5aba9a383f658425a401748311902
Ammar <ammar.alnatsheh@gmail.com>
2019-05-13 13:58:15 -0700

    display player name and start time

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,37 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            playerName: 'X',
+            board: [],
+            flippedCard: 0,
+            timer: 0,
+        }
     }
 
     componentDidMount () {
+        let name = prompt('Enter your name');
+        let time = new Date();
+
+        this.setState({
+            playerName: name,
+            board: [],
+            flippedCard: 0,
+            timer: time, 
+        });
     }
 
     render() {
         return (
         <div className='App'>
-        Hello World
+            <div className="headder">
+                <div className="playerName">
+                    {`Player Name : ${this.state.playerName}`}
+                </div>
+                <div className="timer">
+                    {`Game start at : ${this.state.timer.toLocaleString()}`}
+                </div>
+            </div>
         </div>);
     }
 }
