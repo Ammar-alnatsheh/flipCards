dbfb260a995d45de0a470765bc56d9358098cd70
Ammar <ammar.alnatsheh@gmail.com>
2019-05-14 11:12:03 -0700

    make cards flip

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-var Card = (props) => {
-    let image = `./pics/pic${ props.card.hidden ? 0 : props.card.value }.png`;
-    return (
-        <td className="Card" style={{backgroundImage: `url(${image})`}}>
-        </td>
-    );
-};
+class Card extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value : 0,
+            hidden : true,
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            value : this.props.card.value,
+            hidden : this.props.card.hidden,
+        });
+    }
+
+    onClick () {
+        this.setState({
+            value : this.state.value,
+            hidden : !this.state.hidden,
+        });
+    }
+
+    render() {
+        let image = `./pics/pic${ this.state.hidden ? 0 : this.state.value }.png`;
+        return (
+            <td className="Card" style={{backgroundImage: `url(${image})`}} onClick={this.onClick.bind(this)}>
+            </td>
+        );
+    }
+}
 
 export default Card;
